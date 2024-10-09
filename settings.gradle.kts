b45eb463bff964acc3da91747d5efe7edecbc51c
r8vnhill <reachme@ravenhill.cl>
2024-10-09 10:56:15 -0300

    Add initial project structure and configuration files
    This commit sets up the initial project structure and configuration for a Kotlin project. It includes Gradle wrapper scripts, IDEA project files, code style settings, and the basic directory layout with relevant build scripts and properties. The configuration ensures proper handling of dependencies, JVM options, and Gitignore settings for the project.

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+rootProject.name = "kotest-intro"
+
+pluginManagement {
+    includeBuild("convention-plugins")
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+    }
+}
+
+include(
+    ":basic-tests",
+)
